f0cb691931a23b87557cf02dfdac0417a1de3233
Nick <tnick1502@mail.ru>
2024-07-01 15:39:50 +0300

    feat: work with rc websocket

--- app/api/websockets.py
+++ app/api/websockets.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends
 from starlette.websockets import WebSocketDisconnect
 import asyncio
+import pickle
+
+from services.depends import get_experiments_service, get_s3_service
+from services.experiments import ExperimentsService
+from services.s3 import S3Service
 
 
 router = APIRouter(
@@ -8,22 +13,50 @@ router = APIRouter(
     tags=['resonant_column'])
 
 
-@router.websocket("/ws/")
+@router.websocket("/ws/{experiment_id}/")
 async def websocket_endpoint(
-        websocket: WebSocket
+        experiment_id: int,
+        websocket: WebSocket,
+        service: ExperimentsService = Depends(get_experiments_service),
+        s3_service: S3Service = Depends(get_s3_service),
 ):
+    key = await service.get_file(experiment_id)
+
+    response = await s3_service.get(key[0])
+
+    if 'Body' in response:
+        body = response['Body']
+        # Читаем все данные из тела как байты
+        file_bytes = await body.read()
+
+        # Десериализация данных из байтов
+        experiment_data = pickle.loads(file_bytes)
+
     await websocket.accept()
 
-    G = [99.9497337, 96.44587571, 97.78343486, 96.28324787, 96.15114453, 96.70875337, 87.93178639, 76.15651759,
-         59.93505872, 41.84069101]
-    shear_strain = [8.36201169e-07, 1.83437208e-06, 4.11686470e-06, 8.95700624e-06, 1.98529674e-05, 4.39205547e-05,
-                    9.62087004e-05, 2.14844133e-04, 4.72252237e-04, 1.03690620e-03]
     try:
         while True:
-            for i in range(len(shear_strain)):
-                data = {"timestamp": shear_strain[i], "value": G[i]}
+            for i in experiment_data["result_RCCT"].keys():
+                print(experiment_data["result_RCCT"])
+                for j in range(len(experiment_data["result_RCCT"][i]["Freq"])):
+                    data = {
+                        'x': experiment_data["result_RCCT"][i]["Freq"][j],
+                        "y": experiment_data["result_RCCT"][i]["ShearStrain1[]"][j],
+                    }
+                    await websocket.send_json(data)
+                    await asyncio.sleep(0.5)
+
+                    print(experiment_data["result_RCCT"][i]["Freq"][j])
+
+                data = {
+                    "timestamp": experiment_data['ShearStrain1[]'][i],
+                    "value": experiment_data['G1[MPa]'][i],
+                    "frequency": experiment_data['Frequency1'][i],
+                    "current": experiment_data['CURRENT[A]'][i],
+                }
+
                 await websocket.send_json(data)
-                await asyncio.sleep(3)
+                await asyncio.sleep(0.5)
 
             await websocket.close()
             break
